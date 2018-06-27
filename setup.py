6043e41eb1088e4e3063a7406a9e4fafe4068332
Pawel Lampe <pawel.lampe@gmail.com>
2018-06-27 15:34:32 +0200

    setup extended

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup
 setup(
     name='pytest-timeouts',
     version='0.1.0',
+    description='Linux-only Pytest plugin to control durations of various test case execution phases',
+    author='Pawel Lampe',
+    author_email='pawel.lampe@gmail.com',
+    url='https://github.com/Scony/pytest-timeouts',
+    install_requires=[
+        'pytest',
+    ],
     py_modules=[
         'pytest_timeouts',
     ],
+    entry_points={
+        'pytest11': ['timeouts = pytest_timeouts'],
+    },
 )
